136417f8982d404c50cec745419a7c1c89544e05
Ignat123 <ignat-nee@tut.by>
2015-10-21 00:55:36 +0300

    Добавил скрипт на отбивание Санты

---
+++ Assets/Scripts/Bounce/BounceBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceBehaviour : MonoBehaviour {
+
+	public Vector2 force;		// Сила с которой Санта отбивается от объекта
+
+	// Отвечает за столкновения
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(collision.gameObject.name == "Santa") {
+			collision.gameObject.GetComponent<Rigidbody2D>().AddForce(force);
+		}
+	}
+}
