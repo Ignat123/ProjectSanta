e6879f6baa77715688068720603ba44bca637df6
Ignat123 <ignat-nee@tut.by>
2015-10-25 19:28:54 +0300

    Добавил скрипт для стиков

--- Assets/Scripts/Controls/SticksControll.cs
+++ Assets/Scripts/Controls/SticksControll.cs
@@ -2,20 +2,55 @@
 using System.Collections;
 
 public class SticksControll : MonoBehaviour {
-	
-	private Rigidbody2D leftStickRigidbody, rightStickRigidbody;
-	public float force = 1000;	// Сила удара
 
-	void Start () {
-		leftStickRigidbody = GameObject.Find ("LeftStick").GetComponent<Rigidbody2D> ();
-		rightStickRigidbody = GameObject.Find ("RightStick").GetComponent<Rigidbody2D> ();
-	}
+	public float force = 1000;	// Сила удара
+	public Vector2 leftStickAngles = new Vector2(-44, 84);	// Углы для левого стика
+	public Vector2 rightStickAngles = new Vector2(-84, 44);
+	public GameObject leftStick, rightStick;
+	public bool isLeftStickPressed, isRightStickPressed;	//Отвечает за проверку, нажаты ли клавиши
+	public float timeTick = 0.02f;		// Время в секундах, отвечающая за выполнения операции
+	private float lastFrameTime = 0.0f;	//Время последнего выполнения операции
+	public float leftStickAngle, rightStickAngle;	//Углы поворота стиков
+	public float stickSpeed = 5.0f;
 	
-	// Update is called once per frame
+	void Start() {
+		leftStickAngle = leftStickAngles.x;
+		rightStickAngle = rightStickAngles.y;
+	}
+
 	void Update () {
-		if (Input.GetKeyDown ("left"))
-			leftStickRigidbody.AddTorque(force);
-		if (Input.GetKeyDown ("right")) 
-			rightStickRigidbody.AddTorque(-force);
+		if (Time.time < lastFrameTime + timeTick) return;
+		lastFrameTime = Time.time;
+		isLeftStickPressed = false; isRightStickPressed = false;
+		if (Input.GetKey ("left"))
+			isLeftStickPressed = true;
+		if (Input.GetKey ("right"))
+			isRightStickPressed = true;
+		MoveSticks ();
+	}
+
+	void MoveSticks() {
+		if (isLeftStickPressed && leftStickAngle < leftStickAngles.y)
+			leftStickAngle += stickSpeed;
+		else if (!isLeftStickPressed && leftStickAngle > leftStickAngles.x)
+			leftStickAngle -= stickSpeed;
+
+		if (isRightStickPressed && rightStickAngle > rightStickAngles.x)
+			rightStickAngle -= stickSpeed;
+		else if (!isRightStickPressed && rightStickAngle < rightStickAngles.y)
+			rightStickAngle += stickSpeed;
+
+		if (leftStickAngle < leftStickAngles.x)
+			leftStickAngle = leftStickAngles.x;
+		if (leftStickAngle > leftStickAngles.y)
+			leftStickAngle = leftStickAngles.y;
+
+		if (rightStickAngle < rightStickAngles.x)
+			rightStickAngle = rightStickAngles.x;
+		if (rightStickAngle > rightStickAngles.y)
+			rightStickAngle = rightStickAngles.y;
+
+		leftStick.transform.rotation = Quaternion.Euler (leftStick.transform.rotation.x, leftStick.transform.rotation.y, leftStickAngle);
+		rightStick.transform.rotation = Quaternion.Euler (leftStick.transform.rotation.x, leftStick.transform.rotation.y, rightStickAngle);
 	}
 }
