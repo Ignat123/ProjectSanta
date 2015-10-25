af4cce60d68480cd4705c606c49f3f9ce1c9337a
Ignat123 <ignat-nee@tut.by>
2015-10-25 12:30:04 +0300

    Добавил стики

---
+++ Assets/Scripts/Bounce/AddTorgue.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddTorgue : MonoBehaviour {
+	public GameObject torgueObject, secondTorgueGameObject;
+	public float force;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(collision.gameObject == torgueObject || collision.gameObject == secondTorgueGameObject) {
+			gameObject.GetComponent<Rigidbody2D>().AddTorque(force);
+		}
+	}
+}
---
+++ Assets/Scripts/Controls/SticksControll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SticksControll : MonoBehaviour {
+	
+	private Rigidbody2D leftStickRigidbody, rightStickRigidbody;
+	public float force = 1000;	// Сила удара
+
+	void Start () {
+		leftStickRigidbody = GameObject.Find ("LeftStick").GetComponent<Rigidbody2D> ();
+		rightStickRigidbody = GameObject.Find ("RightStick").GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("left"))
+			leftStickRigidbody.AddTorque(force);
+		if (Input.GetKeyDown ("right")) 
+			rightStickRigidbody.AddTorque(-force);
+	}
+}
