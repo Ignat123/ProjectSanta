574b4649481aecf711745847fca3669276478bae
Ignat123 <ignat-nee@tut.by>
2015-10-27 23:39:54 +0300

    Добавил свечение шариков

---
+++ Assets/Scripts/Environment/LightBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBehaviour : MonoBehaviour {
+	public Sprite firstSprite;
+	public Sprite secondSprite;
+	public GameObject spriteOwner;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.name == "Santa") {
+			Sprite currentSprite = spriteOwner.GetComponent<SpriteRenderer>().sprite;
+			spriteOwner.GetComponent<SpriteRenderer>().sprite = currentSprite == firstSprite ? secondSprite : firstSprite;
+		}
+	}
+}
