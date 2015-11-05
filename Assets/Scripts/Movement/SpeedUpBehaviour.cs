d0aeb6527ccd60bd8aca6f6fdb66a43d7e7ffa56
Ignat123 <ignat-nee@tut.by>
2015-11-05 22:24:08 +0300

    Добавил скрипт на толкание персонажа

---
+++ Assets/Scripts/Movement/SpeedUpBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedUpBehaviour : MonoBehaviour {
+
+	public Vector2 power;
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.name == "Santa") {
+			collision.gameObject.GetComponent<Rigidbody2D>().AddForce(power);
+		}
+	}
+}
