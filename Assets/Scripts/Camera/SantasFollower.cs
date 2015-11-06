6cdfdc17a7d9908d6f9f221f2b99c273a0cef646
Ignat123 <ignat-nee@tut.by>
2015-11-06 18:52:30 +0300

    Добавил скрипт для камеры следующей за Сантой

---
+++ Assets/Scripts/Camera/SantasFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SantasFollower : MonoBehaviour {
+
+	public float minPosition = -6.63f;
+	private GameObject santa;
+
+	void Start() {
+		santa = GameObject.Find ("Santa");
+	}
+
+	void Update () {
+		if (santa.transform.position.y > minPosition)
+			transform.position = new Vector3 (transform.position.x, santa.transform.position.y, transform.position.z);
+	}
+}
